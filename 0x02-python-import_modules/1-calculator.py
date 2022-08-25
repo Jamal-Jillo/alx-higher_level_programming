da04e80350d8dab62fec3889c0b44090ac28e1ee
jay <jayjamal86@gmail.com>
2022-08-25 15:06:44 +0300

    calculator

---
+++ 0x02-python-import_modules/1-calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+a = 10
+b = 5
+print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+print("{:d} / {:d} = {:.2f}".format(a, b, calc.div(a, b)))
