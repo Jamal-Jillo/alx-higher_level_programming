22820deeb4bd1b66cff8e35585ca65fe91a210d5
jay <jayjamal86@gmail.com>
2022-08-25 16:17:22 +0300

    counting arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 import sys
 num = len(sys.argv[1:])
 if num > 1:
-	print("{:d} arguments:".format(num, end=""))
+    print("{:d} arguments:".format(num, end=""))
 elif num == 1:
-	print("{:d} argument:".format(num, end=""))
+    print("{:d} argument:".format(num, end=""))
 else:
-	print("{:d} arguments.".format(num))
-for i in range(1, num+1):
+    print("{:d} arguments.".format(num))
+for i in range(1, num + 1):
     print("{:d}: {:s}".format(i, sys.argv[i]))
