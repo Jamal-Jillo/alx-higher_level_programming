718efdbb7a350c460a55f94fbbc5f5ae303b57af
jay <jayjamal86@gmail.com>
2022-08-27 21:31:39 +0300

    switching

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def switch():
-	a = 89
-	b = 10
-	c = a
-	a = b
-	b = c
-	print("a={:d} - b={:d}".format(a, b))
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
