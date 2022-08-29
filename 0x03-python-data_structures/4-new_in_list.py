bcaa1c86f44610022c5b0aa6af286df2646e3a9a
jay <jayjamal86@gmail.com>
2022-08-29 16:39:11 +0300

    fixed error on 4-new_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= (len(my_list) - 1):
-        return my_list
-    else:
-        for i in range(len(my_list) - 1):
-            if idx == i:
-                my_list[i] = element
-    print(my_list)
+    length = len(my_list)
+
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+    return (new_list)
