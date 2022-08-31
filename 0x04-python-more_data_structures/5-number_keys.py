2110265624eaac421e6fba264026443f52db2de1
jay <jayjamal86@gmail.com>
2022-08-31 21:32:15 +0300

    number of keys bugs

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
-#!/usr/bin/pythone3
+#!/usr/bin/python3
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)
+	return len(a_dictionary)
