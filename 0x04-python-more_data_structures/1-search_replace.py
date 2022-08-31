8fe432b2cf33d2715a6f38f8e8100235526d3a98
jay <jayjamal86@gmail.com>
2022-08-31 17:34:48 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(new_list)):
-        new_list[i] = lambda replace: replace if new_list[i] == search else new_list[i]
+        new_list= map(lambda replace: replace if new_list[i] == search else new_list[i], new_list)
         print(new_list)
  
 search_replace([1, 2, 3, 4, 5], 2, 98)      
