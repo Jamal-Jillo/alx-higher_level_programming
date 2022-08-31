b0b53e76e9993f48a76d8c009964cb2e09661f4f
jay <jayjamal86@gmail.com>
2022-08-31 20:15:44 +0300

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    for i in range(len(list_1)):
+        for x in range(len(list_2)):
+            if list_1[i] == list_2[x]:
+                return list_1[i]
