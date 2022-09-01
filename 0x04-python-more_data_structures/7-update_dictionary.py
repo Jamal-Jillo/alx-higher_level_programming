64aa320bbeb546e3de44738f5d10a870540342d7
jay <jayjamal86@gmail.com>
2022-09-01 09:55:46 +0300

    updating a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from turtle import update
-
-
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
+    a_dictionary[key] = value
     return (a_dictionary)
