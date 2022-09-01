f27c2602a59bd66171231a8e6207719dabfd88f5
jay <jayjamal86@gmail.com>
2022-09-01 11:38:06 +0300

    updating a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    new_list = a_dictionary.copy()
-    if not new_list:
+    if not a_dictionary:
         return (None)
     else:
-        for key in new_list:
-            if new_list[key] == max(new_list.values()):
-                return (key)
+        max_score = max(a_dictionary, key=a_dictionary.get)
+        return (max_score)
