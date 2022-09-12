b7929794873eb5227578c97068cfba1af654317f
jay <jayjamal86@gmail.com>
2022-09-12 20:53:42 +0300

    try and exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            print()
+            return x
+    except (ValueError, TypeError):
+        print()
+        return i
