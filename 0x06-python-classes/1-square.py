7019c72719b6da2557e9362d0a2b4a76bcba0d2f
jay <jayjamal86@gmail.com>
2022-09-14 11:02:17 +0300

    creating classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this module contains a class Square
+    that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """This class defines a square based on 0-square,py"""
+    def __init__(self, size):
+        """This method initializes/constructs the class square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
