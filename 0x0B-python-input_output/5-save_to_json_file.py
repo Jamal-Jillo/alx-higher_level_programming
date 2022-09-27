dd029daa10af170302e7d673728eb03bbc977b23
Jamal <jayjamal86@gmail.com>
2022-09-28 01:36:43 +0300

    loading json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module that conts from_json_string
+    Function
+"""
+
+
+import json
+
+
+def from_json_string(my_obj, filename):
+    """from_json_string Function that writes an object to
+    a txt file using Json representation
+
+    Arguments:
+        my_obj -- object
+        filename -- filename
+
+    Returns:
+        Json representation
+    """
+    with open(filename, 'w') as file:
+        file.write(my_obj)
+    return json.loads(my_obj)
