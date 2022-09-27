9b7764477b67c810fa13a5101111ade5611c57cf
Jamal <jayjamal86@gmail.com>
2022-09-27 23:28:05 +0300

    counting text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,15 @@
 
 
 def write_file(filename="", text=""):
+    """write_file A function that writes and counts the string
+
+    Keyword Arguments:
+        filename -- Name of the file (default: {""})
+        text -- The string that is being added (default: {""})
+
+    Returns:
+        The number of characters written
+    """
     with open(filename, 'w') as file:
         file.write(text)
         count = 0
