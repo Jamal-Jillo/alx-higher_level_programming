b2fb14975d4cea3f400f3324e8cdb0b638d6fbe7
Jamal <jayjamal86@gmail.com>
2022-09-27 23:02:29 +0300

    reading a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is a module that holds def read_file(filename="")
+"""
+
+
+def read_file(filename=""):
+    """read_file Function that reads a textfile
+
+    Keyword Arguments:
+        filename -- name of the file to be read (default: {""})
+    """
+    with open("filename", 'r', encoding="UTF8") as file:
+        file.read()
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+# 0x0B Python input & Output
+`Python`
