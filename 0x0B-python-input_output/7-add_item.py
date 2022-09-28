93efac3bcebe728f83318eda6756d3feebe6281f
Jamal <jayjamal86@gmail.com>
2022-09-28 17:21:01 +0300

    fixed import module

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,36 +7,38 @@
 import json
 import sys
 
+# ! Manually tried importing the functions
+# def save_to_json_file(my_obj, filename):
+#    """from_json_string Function that writes an object to
+#    a txt file using Json representation
 
-def save_to_json_file(my_obj, filename):
-    """from_json_string Function that writes an object to
-    a txt file using Json representation
+#    Arguments:
+#        my_obj -- object
+#        filename -- filename
 
-    Arguments:
-        my_obj -- object
-        filename -- filename
-
-    Returns:
-        Json representation
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+#    Returns:
+#        Json representation
+#    """
+#    with open(filename, 'w') as file:
+#        json.dump(my_obj, file)
 
 
-def load_from_json_file(filename):
-    """load_from_json Loads an object from a Json
-    file
+# def load_from_json_file(filename):
+#    """load_from_json Loads an object from a Json
+#    file
 
-    Arguments:
-        filename -- file name
-    """
-    with open(filename) as file:
-        return json.load(file)
+#   Arguments:
+#   filename -- file name
+#   """
+#   with open(filename) as file:
+#       return json.load(file)
 
 
 if __name__ == '__main__':
-    # save_to_json_file = __import__('5-save_to_json_file')
-    # load_from_json_file = __import__('6-load_from_json_file')
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
+    #  Line was too long hence the need for front slash
 
     try:
         data = load_from_json_file('add_item.json')
