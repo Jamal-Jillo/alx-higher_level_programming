e0097efa09eaa7a61d57efdb23224b2390c147e7
Jamal <jayjamal86@gmail.com>
2022-11-01 16:05:25 +0300

    SQL_introduction

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- A script that inserts a new row into the table first_table (database hbtn_0c_0) with:
+-- id = 89
+-- name = Best School
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
