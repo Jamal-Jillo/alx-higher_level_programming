5f7c196792e045a5c52cf3d250cb5aa5eff450a8
Jamal <jayjamal86@gmail.com>
2022-12-05 11:17:00 +0300

    LINE TOO LONG (82 > 79 characters)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY id ASC""")
     for row in cur.fetchall():
         if row[1][0] == 'N':
             print(row)
